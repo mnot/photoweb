c17817fb278581bc728e218f7e8256c521e4dfb7
Mark Nottingham <mnot@mnot.net>
2026-04-04 15:35:53 +1100

    Move cli into __main__

--- photoweb/__init__.py
+++ photoweb/__init__.py
@@ -28,8 +28,6 @@ THE SOFTWARE.
 """
 
 import json
-from optparse import OptionParser
-import os
 import shutil
 import sys
 
@@ -282,53 +280,3 @@ class PhotoWebber(object):
         "Something has gone horribly wrong."
         sys.stderr.write("FATAL: %s\n" % msg)
         sys.exit(1)
-
-
-def photoweb_cli():
-    "Run command-line photoweb."
-    usage   = "Usage: %prog [options] <dir>"
-    version = "photoweb version %s" % __version__
-    option_parser = OptionParser(usage=usage, version=version)
-    option_parser.set_defaults(
-        template='default',
-        html=False,
-    )
-    option_parser.add_option(
-        "-t",
-        action="store",
-        dest="template",
-        help="template to use"
-    )
-    option_parser.add_option(
-        "-p",
-        action="store",
-        dest="page_title",
-        help="Set the page's title"
-    )
-    option_parser.add_option(
-        "-d",
-        action="append",
-        dest="page_desc",
-        help="Set the page's description"
-    )
-    option_parser.add_option(
-        "-r",
-        action="store_true",
-        dest="reverse",
-        help="Reverse the order of the photos"
-    )
-    (options, args) = option_parser.parse_args()
-    if len(args) < 1:
-        option_parser.error("Please specify at least one directory.")
-
-    try:
-        photoweb = PhotoWebber(options)
-        thisdir = os.getcwd()
-        for photodir in args:
-            photoweb.run(os.path.join(thisdir, photodir))
-    except KeyboardInterrupt:
-        photoweb.error("Interrupted.")
-
-
-if __name__ == '__main__':
-    photoweb_cli()
--- photoweb/__main__.py
+++ photoweb/__main__.py
@@ -27,7 +27,52 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 """
 
-from photoweb import photoweb_cli
+from optparse import OptionParser
+import os
 
-if __name__ == '__main__':
-    photoweb_cli()
+from . import PhotoWebber
+
+def photoweb_cli():
+    "Run command-line photoweb."
+    usage   = "Usage: %prog [options] <dir>"
+    version = "photoweb version %s" % __version__
+    option_parser = OptionParser(usage=usage, version=version)
+    option_parser.set_defaults(
+        template='default',
+        html=False,
+    )
+    option_parser.add_option(
+        "-t",
+        action="store",
+        dest="template",
+        help="template to use"
+    )
+    option_parser.add_option(
+        "-p",
+        action="store",
+        dest="page_title",
+        help="Set the page's title"
+    )
+    option_parser.add_option(
+        "-d",
+        action="append",
+        dest="page_desc",
+        help="Set the page's description"
+    )
+    option_parser.add_option(
+        "-r",
+        action="store_true",
+        dest="reverse",
+        help="Reverse the order of the photos"
+    )
+    (options, args) = option_parser.parse_args()
+    if len(args) < 1:
+        option_parser.error("Please specify at least one directory.")
+
+    try:
+        photoweb = PhotoWebber(options)
+        thisdir = os.getcwd()
+        for photodir in args:
+            photoweb.run(os.path.join(thisdir, photodir))
+    except KeyboardInterrupt:
+        photoweb.error("Interrupted.")
