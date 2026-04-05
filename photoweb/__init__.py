36c53a7cb050f7fe4bf1742a89144caa14efc530
Mark Nottingham <mnot@mnot.net>
2026-04-05 13:10:45 +1000

    bump version

--- photoweb/__init__.py
+++ photoweb/__init__.py
@@ -5,5 +5,5 @@ Photoweb: Templated HTML galleries based on in-photo metadata.
 from .engine import PhotoWebber
 from .exceptions import PhotoWebError
 
-__version__ = "0.5.4"
+__version__ = "0.5.5"
 __all__ = ["PhotoWebber", "PhotoWebError"]
